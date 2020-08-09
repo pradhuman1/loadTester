a95642a1f21f5bd26ab39830a4e766b35cd3627f
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-09 15:23:34 +0530

    first meaningful paint

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # loadTester
+
+
+
+#Usefule articles
+`https://michaljanaszek.com/blog/test-website-performance-with-puppeteer/#firstMeaningfulPaint`
+
+
--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
+    
+    const client = await page.target().createCDPSession();
+    await client.send('Performance.enable');
 
-    await page.goto('https://github.com/');
+    await page.goto('https://web.dev/first-meaningful-paint/');
+    await page.waitFor(1000);
+    const performanceMetrics = await client.send('Performance.getMetrics');
 
-    const performanceTiming = JSON.parse(
-        await page.evaluate(() => JSON.stringify(window.performance.timing))
-    );
+    // const performanceTiming = JSON.parse(
+    //     await page.evaluate(() => JSON.stringify(window.performance.timing))
+    // );
 
-    const startTime = performanceTiming.navigationStart;
+    // const startTime = performanceTiming.navigationStart;
 
 
-    console.log(startTime);
-    console.log(performanceTiming);
+    // console.log(startTime);
+    console.log(performanceMetrics);
 
-    await browser.close();
+    //await browser.close();
 })();
