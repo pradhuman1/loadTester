0929fab5685c7511e564062dc5469834435522e9
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-11 15:29:58 +0530

    first meaningful paint

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const puppeteer = require('puppeteer');
 
 (async () => {
 
-    const browser = await puppeteer.launch({headless:false});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     
     const client = await page.target().createCDPSession();
     await client.send('Performance.enable');
 
     await page.goto('https://web.dev/first-meaningful-paint/');
-    await page.waitFor(1000);
+
+    await page.waitFor(2000);
+
     const performanceMetrics = await client.send('Performance.getMetrics');
+
+    function getTime(name){
+        return performanceMetrics.metrics.find(el => el.name === name).value;
+    }
+
+    function getRelTime(el){
+        return  getTime(el) - getTime('NavigationStart');
+    }
+
+    var firstMeaningfulPaint = getRelTime('FirstMeaningfulPaint');
 
     // const performanceTiming = JSON.parse(
     //     await page.evaluate(() => JSON.stringify(window.performance.timing))
@@ -20,7 +32,8 @@ const puppeteer = require('puppeteer');
 
 
     // console.log(startTime);
-    console.log(performanceMetrics);
+    // console.log(performanceMetrics);
+    console.log("firstMeaningfulPaint"+" : "+firstMeaningfulPaint + "sec");
 
-    //await browser.close();
+    await browser.close();
 })();
