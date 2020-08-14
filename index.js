e942e149ffa65bcce3280aa05f014048a0ae31ca
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-08-15 02:38:12 +0530

    All indexes of testing using lighthouse

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const puppeteer = require('puppeteer');
+const lighthouse = require('lighthouse');
 
 (async () => {
+
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     
     const client = await page.target().createCDPSession();
     await client.send('Performance.enable');
+
+    const lighthouseConfig = {
+        port:(
+            new URL(browser.wsEndpoint())
+        ).port
+    }
+    const auditConfig = {
+        extends: 'lighthouse:default',
+        settings: {
+            onlyAudits: [
+            'first-meaningful-paint',
+            'speed-index',
+            'first-cpu-idle',
+            'interactive',
+            'total-blocking-time',
+            'largest-contentful-paint'
+            ],
+        },
+    }
+    const {lhr:{audits}} = await lighthouse(
+        'https://github.com/addyosmani/puppeteer-webperf#first-contentful-paint',
+        lighthouseConfig,
+        auditConfig
+    )
 
     await page.goto('https://github.com/addyosmani/puppeteer-webperf#first-contentful-paint');
 
@@ -56,7 +82,18 @@ const puppeteer = require('puppeteer');
 
     var firstMeaningfulPaint = getRelTime('FirstMeaningfulPaint');
 
-
+    const largestContentful = audits['largest-contentful-paint']['displayValue'];
+    const speedIndex = audits['speed-index']['displayValue'];
+    const blockingTime = audits['total-blocking-time']['displayValue'];
+    const firstCpuIdle = audits['first-cpu-idle']['displayValue'];
+    const interactive = audits['interactive']['displayValue'];
+    
+    console.log("Largest Contentful Paint"+" : "+largestContentful +"sec");
+    console.log("Speed Index"+" : "+speedIndex +"sec");
+    console.log("Total Blocking Time"+" : "+ blockingTime+"sec");
+    console.log("First CPU Idle"+" : "+firstCpuIdle +"sec");
+    console.log("Time to Interactive"+" : "+ interactive+"sec");
+    
     console.log("firstPaint"+" : "+firstPaint[0].startTime/1000+ "sec");
     console.log("firstContentfulPaint"+" : "+firstContentfulPaint[0].startTime/1000+ "sec");
     console.log("firstMeaningfulPaint"+" : "+firstMeaningfulPaint + "sec");
